05797e32b1eae5ca756f3e20f361e4fb88828989
zb2b <dapzy.z@gmail.com>
2021-09-13 18:28:08 +0900

    testing move action

--- public/project.js
+++ public/project.js
@@ -16,6 +16,8 @@ var config = {
     parent: 'canvas',
     pixelArt: true
 };
+const hitarea = [];
+let player;
 const game = new Phaser.Game(config);
 function preload ()
 {
@@ -30,11 +32,31 @@ function create () {
         repeat: -1
     };
     this.anims.create(idleConfig);
-    const player = this.physics.add.sprite(0, 0, 'idle').play('idle');
+    player = this.physics.add.sprite(0, 0, 'idle').play('idle');
     player.scale = 2;
     player.body.setSize(28, 33).setOffset(0, 0);
     player.setOrigin(0);
+    hitarea[0] = this.add.rectangle(100, 400, 200, 800, 0x00ffff, 0.5);
+    hitarea[1] = this.add.rectangle(300, 400, 200, 800, 0xffff00, 0.5);
+    hitarea[0].setInteractive().on('pointerdown', ()=>{
+        player.setVelocityX(-100);
+    });
+    hitarea[1].setInteractive().on('pointerdown', ()=>{
+        player.setVelocityX(100);
+    });
+    hitarea[0].setInteractive().on('pointerout', ()=>{
+        player.setVelocityX(0);
+    });
+    hitarea[1].setInteractive().on('pointerout', ()=>{
+        player.setVelocityX(0);
+    });
+    hitarea[0].setInteractive().on('pointerup', ()=>{
+        player.setVelocityX(0);
+    });
+    hitarea[1].setInteractive().on('pointerup', ()=>{
+        player.setVelocityX(0);
+    });
 }
+const Axis = { x: 0, y: 0 };
 function update () {
-
 }
