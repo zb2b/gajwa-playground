a7fc8b5436b36f267393162cc5d4203c4523597e
zb2b <dapzy.z@gmail.com>
2021-10-20 22:00:42 +0900

    testing

---
+++ README.md
@@ -0,0 +1,22 @@
+# Gajwa Playground
+<hr>
+
+## The shade of myzy
+
+This is Gajwa-playground project.
+Base on Phaser.js
+
+### version info
+- ver 0.0.1
+
+#### Start server
+- server.js
+
+#### Pages in views folder
+- index.ejs
+
+#### Resources in public folder
+- images
+- master.css
+- phaser.js
+- project.js (main script)
--- public/project.js
+++ public/project.js
@@ -1,3 +1,9 @@
+var font = new FontFaceObserver('dgm');
+
+font.load().then(function () {
+    document.documentElement.className += " fonts-loaded";
+});
+
 var config = {
     type: Phaser.AUTO,
     width: 400,
@@ -16,47 +22,67 @@ var config = {
     parent: 'canvas',
     pixelArt: true
 };
-const hitarea = [];
-let player;
+const fontConfig = {font: '32px "dgm"', color: '#fff'};
 const game = new Phaser.Game(config);
+let gameStatus = 'title';
+let line;
+const ui = {};
 function preload ()
 {
     this.load.spritesheet('idle', 'image/idle.png', {frameWidth: 28, frameHeight: 33, endFrame: 10 });
+    this.load.json('line', 'data/line.json');
 }
 const gameManager = {status: 'ready'};
 function create () {
-    let idleConfig = {
-        key: 'idle',
-        frames: this.anims.generateFrameNumbers('idle'),
-        frameRate: 8,
-        repeat: -1
-    };
-    this.anims.create(idleConfig);
-    player = this.physics.add.sprite(0, 0, 'idle').play('idle');
-    player.scale = 2;
-    player.body.setSize(28, 33).setOffset(0, 0);
-    player.setOrigin(0);
-    hitarea[0] = this.add.rectangle(100, 400, 200, 800, 0x00ffff, 0.5);
-    hitarea[1] = this.add.rectangle(300, 400, 200, 800, 0xffff00, 0.5);
-    hitarea[0].setInteractive().on('pointerdown', ()=>{
-        player.setVelocityX(-100);
-    });
-    hitarea[1].setInteractive().on('pointerdown', ()=>{
-        player.setVelocityX(100);
-    });
-    hitarea[0].setInteractive().on('pointerout', ()=>{
-        player.setVelocityX(0);
-    });
-    hitarea[1].setInteractive().on('pointerout', ()=>{
-        player.setVelocityX(0);
-    });
-    hitarea[0].setInteractive().on('pointerup', ()=>{
-        player.setVelocityX(0);
-    });
-    hitarea[1].setInteractive().on('pointerup', ()=>{
-        player.setVelocityX(0);
+    function playerActions() {
+        const hitarea = [];
+        let player;
+        let idleConfig = {
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('idle'),
+            frameRate: 8,
+            repeat: -1
+        };
+        this.anims.create(idleConfig);
+        player = this.physics.add.sprite(0, 0, 'idle').play('idle');
+        player.scale = 2;
+        player.body.setSize(28, 33).setOffset(0, 0);
+        player.setOrigin(0);
+        hitarea[0] = this.add.rectangle(100, 400, 200, 800, 0x00ffff, 0.5);
+        hitarea[1] = this.add.rectangle(300, 400, 200, 800, 0xffff00, 0.5);
+        hitarea[0].setInteractive().on('pointerdown', ()=>{
+            player.setVelocityX(-100);
+        });
+        hitarea[1].setInteractive().on('pointerdown', ()=>{
+            player.setVelocityX(100);
+        });
+        hitarea[0].setInteractive().on('pointerout', ()=>{
+            player.setVelocityX(0);
+        });
+        hitarea[1].setInteractive().on('pointerout', ()=>{
+            player.setVelocityX(0);
+        });
+        hitarea[0].setInteractive().on('pointerup', ()=>{
+            player.setVelocityX(0);
+        });
+        hitarea[1].setInteractive().on('pointerup', ()=>{
+            player.setVelocityX(0);
+        });
+    }
+
+    line = this.cache.json.get('line').story.line[0];
+    console.log(line);
+
+    ui.startgame = this.add.text(0, 0, 'Start Game', fontConfig);
+    ui.startgame.x = 200;
+    ui.startgame.y = 400;
+    ui.startgame.setOrigin(0.5);
+    ui.startgame.setInteractive();
+    ui.startgame.on('pointerup', function () {
+        gameStatus = 'level-0';
+        console.log(gameStatus);
+        ui.startgame.setVisible(false);
     });
 }
-const Axis = { x: 0, y: 0 };
 function update () {
 }
