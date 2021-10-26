52ea4fedfe66c793a3186d65a35061ed4ef23e97
zb2b <dapzy.z@gmail.com>
2021-10-26 19:53:22 +0900

    update

--- public/project.js
+++ public/project.js
@@ -1,13 +1,12 @@
 var font = new FontFaceObserver('dgm');
-
 font.load().then(function () {
     document.documentElement.className += " fonts-loaded";
 });
-
-var config = {
+const display = {width : 360, height: 680};
+const config = {
     type: Phaser.AUTO,
-    width: 400,
-    height: 800,
+    width: display.width,
+    height: display.height,
     physics: {
         default: 'arcade',
         arcade: {
@@ -27,62 +26,69 @@ const game = new Phaser.Game(config);
 let gameStatus = 'title';
 let line;
 const ui = {};
+const timer = { shaker: null };
 function preload ()
 {
-    this.load.spritesheet('idle', 'image/idle.png', {frameWidth: 28, frameHeight: 33, endFrame: 10 });
     this.load.json('line', 'data/line.json');
 }
 const gameManager = {status: 'ready'};
 function create () {
-    function playerActions() {
-        const hitarea = [];
-        let player;
-        let idleConfig = {
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('idle'),
-            frameRate: 8,
-            repeat: -1
-        };
-        this.anims.create(idleConfig);
-        player = this.physics.add.sprite(0, 0, 'idle').play('idle');
-        player.scale = 2;
-        player.body.setSize(28, 33).setOffset(0, 0);
-        player.setOrigin(0);
-        hitarea[0] = this.add.rectangle(100, 400, 200, 800, 0x00ffff, 0.5);
-        hitarea[1] = this.add.rectangle(300, 400, 200, 800, 0xffff00, 0.5);
-        hitarea[0].setInteractive().on('pointerdown', ()=>{
-            player.setVelocityX(-100);
-        });
-        hitarea[1].setInteractive().on('pointerdown', ()=>{
-            player.setVelocityX(100);
-        });
-        hitarea[0].setInteractive().on('pointerout', ()=>{
-            player.setVelocityX(0);
-        });
-        hitarea[1].setInteractive().on('pointerout', ()=>{
-            player.setVelocityX(0);
-        });
-        hitarea[0].setInteractive().on('pointerup', ()=>{
-            player.setVelocityX(0);
-        });
-        hitarea[1].setInteractive().on('pointerup', ()=>{
-            player.setVelocityX(0);
-        });
-    }
 
     line = this.cache.json.get('line').story.line[0];
     console.log(line);
-
     ui.startgame = this.add.text(0, 0, 'Start Game', fontConfig);
-    ui.startgame.x = 200;
-    ui.startgame.y = 400;
-    ui.startgame.setOrigin(0.5);
-    ui.startgame.setInteractive();
+    InitObject(ui.startgame, display.width * 0.5, display.height * 0.5, 0.5, true);
     ui.startgame.on('pointerup', function () {
         gameStatus = 'level-0';
         console.log(gameStatus);
-        ui.startgame.setVisible(false);
+        game.scene.scenes[0].tweens.add({
+            targets: ui.startgame,
+            alpha: 0,
+            duration: 800,
+        });
+        setTimeout(()=> {
+            this.setVisible(false);
+            ui.lines[0].setVisible(true);
+            ShakeObject(ui.lines[0], 20, 20, 240);
+
+        }, 1200);
+        //ShakeObject(ui.startgame, 20, 20, 240);
     });
+    ui.lines = [];
+    ui.lines.push(this.add.text(0, 0, line[0], fontConfig).setVisible(false));
+    InitObject(ui.lines[0], display.width * 0.5, display.height * 0.5, 0.5,5, false);
+    if(gameStatus === 'level-0') console.log('log');
+
 }
 function update () {
+
+}
+function InitObject(obj, x, y, origin, setInteractive) {
+    obj.x = x;
+    obj.y = y;
+    obj.setOrigin(origin);
+    if(setInteractive) obj.setInteractive();
+}
+function RandomPlusMinus() {
+    return (Math.random() > 0.5) ? 1 : -1;
+}
+function RandomizePos(obj, x, y, min, max) {
+    obj.x = x + min + Math.random() * max * RandomPlusMinus();
+    obj.y = y + min + Math.random() * max * RandomPlusMinus();
+}
+function ShakeObject(obj, max, speed, time) {
+    if(timer.shaker !== null) return;
+    var x = obj.x;
+    var y = obj.y;
+    setTimeout(()=> {
+        clearTimeout(timer.shaker);
+        timer.shaker = null;
+        obj.x = x;
+        obj.y = y;
+    }, time);
+    repeat();
+    function repeat() {
+        RandomizePos(obj, x, y, 0, max);
+        timer.shaker = setTimeout(repeat, speed);
+    }
 }
