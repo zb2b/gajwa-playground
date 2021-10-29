8cb316b13733314810483939b423e5084b6e14ce
zb2b <dapzy.z@gmail.com>
2021-10-29 16:15:52 +0900

    save before webpack update

--- public/project.js
+++ public/project.js
@@ -3,6 +3,22 @@ font.load().then(function () {
     document.documentElement.className += " fonts-loaded";
 });
 const display = {width : 360, height: 680};
+function device(){
+    let varUA = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
+
+    if ( varUA.indexOf('android') > -1) {
+        return "안드로이드";
+    } else if ( varUA.indexOf("iphone") > -1||varUA.indexOf("ipad") > -1||varUA.indexOf("ipod") > -1 ) {
+        return "아이폰";
+    } else if ( varUA.indexOf("mac") > -1) {
+        return "맥";
+    } else if ( varUA.indexOf("windows") > -1) {
+        return "컴퓨터";
+    } else {
+        return "기계";
+    }
+}
+
 const config = {
     type: Phaser.AUTO,
     width: display.width,
@@ -45,7 +61,8 @@ let gameStatus = {
     },
 };
 let myLog = {text: null};
-let line = [];
+const chapterTitle = [];
+const line = { story: [], chapter: [] };
 const ui = {};
 const timer = { shaker: null };
 function preload ()
@@ -59,7 +76,13 @@ function create () {
     // TODO: LOG TEXT
     myLog = this.add.text(8, 8, 'main-0', fontConfig).setFontSize(16);
 
-    line = this.cache.json.get('line').story.line[0];
+    // TODO: JSON LINES TO ARRAY
+    let lineJson = this.cache.json.get('line');
+    for (let l = 0; l < lineJson.chapter.length; l++) {
+        chapterTitle.push(lineJson.chapter[l]);
+    }
+    // line Json 첫번째 배열: 챕터별 라인, 두번째 배열: 라인 당 개별 대사
+    line.story = lineJson.line.story;
     console.log(line);
 
     ui.startgame = this.add.text(0, 0, 'Start Game', fontConfig);
@@ -70,36 +93,53 @@ function create () {
             targets: ui.startgame,
             alpha: 0,
             duration: 800,
+            onComplete: () => gameStatus.chapter = 'story'
         });
-        setTimeout(()=> {
-            // 1200 뒤에 스토리 시작
-            gameStatus.chapter = 'story';
-        }, 1200);
     });
-    ui.lines = [];
-    ui.lines.push(this.add.text(0, 0, line[0], fontConfig).setVisible(false));
-    InitObject(ui.lines[0], display.width * 0.5, display.height * 0.5, 0.5,5, true);
-    ui.lines[0].setAlign('center');
-    ui.lines[0].on('pointerup', function () {
+    // TODO: 스토리 시작 메인 텍스트 설정
+    ui.mainText = this.add.text(0, 0, line.story[0][0], fontConfig).setVisible(false).setAlign('center');
+    InitObject(ui.mainText, display.width * 0.5, display.height * 0.5, 0.5,true);
+    ui.mainText.on('pointerup', function () {
         gameStatus.idx++;
-        this.text = line[gameStatus.idx];
+        this.text = line.story[0][gameStatus.idx];
         ShakeObject(this, 20, 20, 240);
-        if(gameStatus.idx >= line.length) {
+        if(gameStatus.idx >= line.story[0].length) {
             gameStatus.chapter = 'chapter-1';
             gameStatus.idx = 0;
         }
     });
+    // TODO: 챕터 타이틀 텍스트 설정
+    ui.chapterText = this.add.text(0, 0, chapterTitle[0], fontConfig).setVisible(false).setAlign('center');
+    InitObject(ui.chapterText, display.width * 0.5, display.height * 0.5, 0.5,true);
+
+    // TODO: 메인 대화창 텍스트 설정
+    ui.talkText = this.add.text(0, 0, line.story[1][0], fontConfig).setVisible(true).setAlign('left').setFontSize(16);
+    InitObject(ui.talkText, 20, display.height - 40, 0,true);
 }
 function story() {
     // TODO: 스토리 시작
     console.log('스토리 시작');
-    ui.lines[0].setVisible(true);
-    ShakeObject(ui.lines[0], 20, 20, 240);
+    ui.mainText.setVisible(true);
+    ShakeObject(ui.mainText, 20, 20, 240);
 }
 function chapter(chapterNum) {
     // TODO: 챕터 시작
     console.log('챕터', chapterNum, '시작');
-    ui.lines[0].setVisible(false);
+    ui.mainText.setVisible(false);
+    ui.chapterText.setVisible(true);
+    ui.chapterText.alpha = 0;
+    game.scene.scenes[0].tweens.add({
+        targets: ui.chapterText,
+        alpha: 1,
+        duration: 2000,
+        yoyo: true,
+        ease: Phaser.Math.Easing.Elastic.InOut,
+        onComplete: startChapter
+    });
+    function startChapter() {
+        gameStatus.idx++;
+
+    }
 }
 
 function update () {
@@ -120,8 +160,8 @@ function RandomizePos(obj, x, y, min, max) {
 }
 function ShakeObject(obj, max, speed, time) {
     if(timer.shaker !== null) return;
-    var x = obj.x;
-    var y = obj.y;
+    const x = obj.x;
+    const y = obj.y;
     setTimeout(()=> {
         clearTimeout(timer.shaker);
         timer.shaker = null;
