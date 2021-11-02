c9c677fd64490d986a6acf8d4983ba9efc5bf52c
zb2b <dapzy.z@gmail.com>
2021-11-02 15:18:29 +0900

    save after webpack update

--- public/webpack/app.js
+++ public/webpack/app.js
@@ -6,7 +6,7 @@ var __webpack_exports__ = {};
 // const FontFaceObserver = require('fontfaceobserver');
 let font = new FontFaceObserver('dgm');
 font.load().then(function () {
-    console.log('font' ,font.family , 'loaded');
+    //console.log('font' ,font.family , 'loaded');
 });
 
 const display = {width : 360, height: 680};
@@ -33,7 +33,7 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            //debug: true
+            debug: true
         }
     },
     scene: {
@@ -49,6 +49,7 @@ const game = new Phaser.Game(config);
 // TODO: 게임 상태 변경
 let gameStatus = {
     Chapter: 'load',
+    ChapterNum: 0,
     Index: 0,
     get chapter(){
         return this.Chapter;
@@ -56,7 +57,10 @@ let gameStatus = {
     set chapter(value) {
         this.Chapter = value;
         if(value === 'story') story();
-        else if(value.split("-")[0] === 'chapter') chapter(value.split("-")[1]);
+        else if(value.split("-")[0] === 'chapter') {
+            this.ChapterNum = value.split("-")[1];
+            chapter(this.ChapterNum);
+        }
         myLog.text = this.Chapter + "-" + this.Index;
     },
     get idx(){
@@ -65,125 +69,225 @@ let gameStatus = {
     set idx(value) {
         this.Index = value;
         myLog.text = this.Chapter + "-" + this.Index;
+        if(this.chapter === 'story'){
+            if(this.idx >= line.story[0].length) {
+                this.chapter = 'chapter-1';
+                this.idx = 0;
+                ui.skip.disableInteractive().setVisible(false);
+            }
+        }
     },
+    get chapterNum(){
+        return this.ChapterNum;
+    }
 };
 let myLog = {text: null};
 const mainObjConfig = {characterMove: false};
 const mainObject = {};
+const otherObject = {};
 const touch = {x: 0, y: 0};
-const chapterTitle = [];
-const line = { story: [], chapter: [] };
+const line = { story: [], chapter: [], task: [] };
 const ui = {};
 const timer = { shaker: null };
+
 // TODO: * 프리로드
-function preload ()
-{
+function preload () {
+    // Data
     this.load.json('line', 'data/line.json');
+
+    // UI
+    this.load.image('map', 'image/map-desert.png');
+
+    // maps
     this.load.image('talkbox', 'image/talkbox.png');
+
+    // character animation
     this.load.spritesheet('stand', 'image/stand.png', { frameWidth: 32, frameHeight: 32, endFrame: 1 });
     this.load.spritesheet('run', 'image/run.png', { frameWidth: 32, frameHeight: 32, endFrame: 3 });
     this.load.spritesheet('seek', 'image/seek.png', { frameWidth: 32, frameHeight: 32, endFrame: 1 });
-// plugin load
+
+    // another animation
+    this.load.spritesheet('blink', 'image/blink.png', { frameWidth: 16, frameHeight: 16, endFrame: 1 });
+
+    // plugins
     var url_nine = 'https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexninepatchplugin.min.js';
     this.load.plugin('rexninepatchplugin', url_nine, true);
 }
-const gameManager = {status: 'ready'};
 function create () {
     gameStatus.chapter = 'main';
     gameStatus.idx = 0;
-    // TODO: LOG TEXT
-    myLog = this.add.text(8, 8, 'main-0', fontConfig).setFontSize(16);
+
+    // TODO: 로그 및 디버그
+    const LogContainer = this.add.container(0, 0).setVisible(false);
+    myLog = this.add.text(6, 2, 'main-0', fontConfig).setFontSize(16);
+    const myLogBox = this.add.rectangle(display.width * 0.5, 8, display.width, 24, 0x333333, 1);
+    LogContainer.add([myLogBox, myLog]);
+    this.physics.world.drawDebug = false;
+    this.input.keyboard.addKey('TAB').on('down', function(event) {
+        LogContainer.setVisible(true);
+        game.scene.scenes[0].physics.world.drawDebug = true;
+    }).on('up', function(event) {
+        LogContainer.setVisible(false);
+        game.config.physics.arcade.drawDebug = true;
+        game.scene.scenes[0].physics.world.drawDebug = false;
+        game.scene.scenes[0].physics.world.debugGraphic.clear();
+    });
+
     // 터치 포인터 위치 리턴
+    function charMove(pos) {
+        touch.x = pos.x;
+        touch.y = pos.y;
+        mainObject.character.setFlipX(pos.x < mainObject.character.x);
+        game.scene.scenes[0].physics.moveToObject(mainObject.character, {x: pos.x, y: pos.y - 32}, 180);
+        mainObject.character.play('run');
+    }
     this.input.on('pointerup', function (pointer)
     {
-        touch.x = pointer.x;
-        touch.y = pointer.y;
-        function charMove() {
-            mainObject.character.setFlipX(pointer.x < mainObject.character.x);
-            game.scene.scenes[0].physics.moveToObject(mainObject.character, {x: pointer.x, y: pointer.y - 32}, 180);
-            mainObject.character.play('run');
-        }
-        if(mainObjConfig.characterMove === true) charMove();
+        // if(pointer.y < 120) return;
+        if(mainObjConfig.characterMove === true) charMove(pointer);
     }, this);
 
     // TODO: JSON LINES TO ARRAY
     let lineJson = this.cache.json.get('line');
     for (let l = 0; l < lineJson.chapter.length; l++) {
-        chapterTitle.push(lineJson.chapter[l]);
+        line.chapter.push(lineJson.chapter[l]);
+    }
+    for (let c = 0; c < lineJson.task.length; c++) {
+        line.task.push(lineJson.task[c]);
     }
     // line Json 첫번째 배열: 챕터별 라인, 두번째 배열: 라인 당 개별 대사
     line.story = lineJson.line.story;
     console.log(line);
 
-    ui.startgame = this.add.text(0, 0, 'Start Game', fontConfig);
-    InitObject(ui.startgame, display.width * 0.5, display.height * 0.5, 0.5, true);
-    ui.startgame.on('pointerup', function () {
-        this.disableInteractive();
-        game.scene.scenes[0].tweens.add({
-            targets: ui.startgame,
-            alpha: 0,
-            duration: 800,
-            onComplete: () => gameStatus.chapter = 'story'
-        });
-    });
+    ui.startgame = this.add.text(0, 0, 'SHADE OF MYZY\nSTART', fontConfig).setAlign('center');
+    InitObject(ui.startgame, display.width * 0.5, display.height * 0.5, 0.5, false);
+
     // TODO: 스토리 시작 메인 텍스트 설정
     ui.mainText = this.add.text(0, 0, line.story[0][0], fontConfig).setVisible(false).setAlign('center');
     InitObject(ui.mainText, display.width * 0.5, display.height * 0.5, 0.5,true);
-    ui.mainText.on('pointerup', function () {
-        gameStatus.idx++;
-        this.text = line.story[0][gameStatus.idx];
-        ShakeObject(this, 20, 20, 240);
-        if(gameStatus.idx >= line.story[0].length) {
-            gameStatus.chapter = 'chapter-1';
-            gameStatus.idx = 0;
+
+    // TODO: 전체화면 스킵 오브젝트
+    ui.skip = this.add.rectangle(display.width * 0.5, display.height * 0.5, display.width, display.height, 0x00ff00, 0).setVisible(true);
+    ui.skip.setInteractive().setVisible(true);
+    ui.skip.on('pointerup', function () {
+        if(gameStatus.chapter === 'main'){
+            this.setVisible(false);
+            game.scene.scenes[0].tweens.add({
+                targets: ui.startgame,
+                alpha: 0,
+                duration: 1200,
+                onComplete: () => {
+                    gameStatus.chapter = 'story';
+                    this.setVisible(true);
+                }
+            });
+            return;
         }
+        gameStatus.idx++;
+        ui.mainText.text = line.story[0][gameStatus.idx];
+        ShakeObject(ui.mainText, 20, 20, 240);
     });
     // TODO: 챕터 타이틀 텍스트 설정
-    ui.chapterText = this.add.text(0, 0, chapterTitle[0], fontConfig).setVisible(false).setAlign('center');
+    ui.chapterText = this.add.text(0, 0, line.chapter[0], fontConfig).setVisible(false).setAlign('center');
     InitObject(ui.chapterText, display.width * 0.5, display.height * 0.5, 0.5,true);
 
     // TODO: 메인 대화창 설정
-    ui.talkBox = this.add.rexNinePatch({
-        x: display.width * 0.5, y: display.height - 120,
+    ui.talkBox = this.add.container(display.width * 0.5, display.height * 0.5 + 140).setVisible(false);
+    ui.talkText = this.add.text(-display.width * 0.5 + 40, 32, '', fontConfig).setAlign('left').setFontSize(16).setLineSpacing(8);
+    ui.talkBackground = this.add.rexNinePatch({
+        x: 0, y: 0,
         width: display.width - 20, height: 180,
         key: 'talkbox',
         columns: [16, undefined, 16],
         rows: [16, undefined, 16],
-    }).setVisible(false).setInteractive();
-    ui.talkBox.on('pointerup', function () {
+    }).setOrigin(0.5, 0).setInteractive();
+    ui.talkBox.add([ui.talkBackground, ui.talkText]);
+
+    ui.talkBackground.on('pointerup', function () {
+        talk();
+    });
+    function talk() {
         game.scene.scenes[0].time.removeAllEvents();
-        if(line.story[1].length - 1 > gameStatus.idx) {
+        if(line.story[gameStatus.chapterNum].length - 1 > gameStatus.idx) {
             gameStatus.idx++;
-            if(gameStatus.idx === 4) mainObject.character.play('seek');
+            if(line.story[1][gameStatus.idx] === 'close'){
+                setTimeout(() => mainObjConfig.characterMove = true, 60);
+                setTimeout(function () {
+                    if(gameStatus.idx < 6){otherObject.blink.setVisible(true).play('blink');}
+                }, 2400);
+                ui.talkBox.setVisible(false);
+                ui.taskBox.setVisible(true);
+            }
+            if(gameStatus.idx === 2) {
+                game.scene.scenes[0].tweens.killTweensOf(mainObject.character);
+                game.scene.scenes[0].tweens.add({
+                    targets: mainObject.character,
+                    alpha: 0.2,
+                    duration: 3200
+                });
+            }
+            else if(gameStatus.idx === 3) {
+                game.scene.scenes[0].tweens.killTweensOf(mainObject.character);
+                game.scene.scenes[0].tweens.add({
+                    targets: mainObject.character,
+                    alpha: 1,
+                    duration: 400
+                });
+            }
+            else if(gameStatus.idx === 4) mainObject.character.play('seek');
             else if(gameStatus.idx === 5) mainObject.character.play('stand');
         }
         else {
-            setTimeout(() => mainObjConfig.characterMove = true, 60);
-            ui.talkText.setVisible(false);
-            ui.talkBox.setVisible(false);
-            ui.taskText.setVisible(true);
+            console.log('null index line');
         }
         ui.talkText.text = '';
         typewriteText(ui.talkText, line.story[1][gameStatus.idx], 60);
-    });
-    ui.talkText = this.add.text(0, 0, '', fontConfig).setVisible(true).setAlign('left').setFontSize(16);
-    InitObject(ui.talkText, 40, display.height - 180, 0,false);
+    }
+
     // TODO: 미션 텍스트
-    ui.taskText = this.add.text(0, 0, '목소리가 나는 곳을 찾아보자.', fontConfig).setVisible(false).setAlign('left').setFontSize(16);
-    InitObject(ui.taskText, display.width * 0.5, 60, 0.5,false);
+    ui.taskBox = this.add.container(display.width * 0.5, 60).setVisible(false);
+    ui.taskText = this.add.text(0, 0, line.task[0], fontConfig).setOrigin(0.5).setFontSize(16);
+    ui.taskBackground = this.add.rectangle(0, 0, display.width, 32, 0x000000).setOrigin(0.5);
+    ui.taskBox.add([ui.taskBackground, ui.taskText]);
+
     // TODO: 캐릭터 생성
     setAnims();
-    mainObject.character = this.physics.add.sprite(display.width * 0.5, display.height * 0.5, 'stand')
+    mainObject.character = this.physics.add.sprite(display.width * 0.5, display.height * 0.5 - 30, 'stand')
         .play('stand').setScale(2).setVisible(false);
 
+    ui.bg = this.add.sprite(display.width * 0.5, display.height * 0.5, 'map').setScale(2).setVisible(false);
+
+    // TODO: 기타 효과 오브젝트 생성
+    otherObject.blink = this.physics.add.sprite(display.width * 0.5 + 80, display.height * 0.5 - 10, 'blink')
+        .setScale(2).setVisible(false);
+    this.physics.add.overlap(mainObject.character, otherObject.blink, onCol, null, this);
+
+    // TODO: 물리 설정
+    function onCol(player, obj) {
+        if(obj === otherObject.blink){
+            charMove({x: obj.x, y: obj.y});
+            obj.disableBody(true, true);
+            mainObjConfig.characterMove = false;
+            ui.talkBox.setVisible(true);
+            ui.taskBox.setVisible(false);
+            talk();
+        }
+    }
+
+    // 레이어 정리
     mainObject.layer = this.add.layer();
+    mainObject.layer.add(ui.bg);
     mainObject.layer.add(mainObject.character);
+    mainObject.layer.add(otherObject.blink);
     mainObject.layer.add(ui.talkBox);
-    mainObject.layer.add(ui.talkText);
-    mainObject.layer.add(ui.taskText);
+    mainObject.layer.add(ui.taskBox);
+    mainObject.layer.add(ui.skip);
+    mainObject.layer.add(ui.bg);
+    mainObject.layer.add(LogContainer);
 }
-// TODO: 애니메이션 추가
+
 function setAnims() {
+    // TODO: 애니메이션 추가
     var scene = game.scene.scenes[0];
     scene.anims.create({
         key: 'stand',
@@ -203,6 +307,12 @@ function setAnims() {
         frameRate: 2,
         repeat: -1
     });
+    scene.anims.create({
+        key: 'blink',
+        frames: scene.anims.generateFrameNumbers('blink', { start: 0, end: 1, first: 0 }),
+        frameRate: 2,
+        repeat: -1
+    });
 }
 function story() {
     // TODO: 스토리 시작
@@ -216,6 +326,7 @@ function chapter(chapterNum) {
     ui.mainText.setVisible(false);
     ui.chapterText.setVisible(true);
     ui.chapterText.alpha = 0;
+    // 챕터 타이틀 애니메이션
     game.scene.scenes[0].tweens.add({
         targets: ui.chapterText,
         alpha: 1,
@@ -224,8 +335,10 @@ function chapter(chapterNum) {
         ease: Phaser.Math.Easing.Elastic.InOut,
         onComplete: startChapter
     });
+    // 챕터 시작
     function startChapter() {
         ui.talkBox.setVisible(true);
+        ui.bg.setVisible(true);
         mainObject.character.setVisible(true).setAlpha(0);
         game.scene.scenes[0].tweens.add({
             targets: mainObject.character,
